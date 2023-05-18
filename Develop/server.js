db39ff7faab17286ab08a0c03175214ade7b1d23
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-05-18 18:17:00 -0500

    Create Express app and set up middleware.

--- Develop/server.js
+++ Develop/server.js
@@ -1,2 +1,15 @@
+// Import necessary modules
 const express = require('express');
 const path = require('path');
+
+// Create an Express app and set up middleware
+const app = express();
+
+// Middleware to parse JSON equests
+app.use(express.json());
+
+// Middleware to serve static files
+app.use(express.static('public'));
+
+
+
