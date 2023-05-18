b01cc66150e150ba00012396056011407d2bd8cb
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-05-18 18:18:38 -0500

    Set up server to listen on port.

--- Develop/server.js
+++ Develop/server.js
@@ -21,5 +21,8 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
-  
+  const PORT = 3000;
 
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
