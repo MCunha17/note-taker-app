414a573700a550ef827af8af302c726a9b21ca5b
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-05-18 22:38:23 -0500

    Updated unique ID generation method.

--- Develop/server.js
+++ Develop/server.js
@@ -1,33 +1,32 @@
-// import modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-// create Express app and set up middleware
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// middleware to parse JSON equests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // HTML routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  // send the notes.html file when the /notes route is accessed
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res) => {
+  // send the index.html file when the root route is accessed
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // API routes
 app.get('/api/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+  // read the db.json file and return all saved notes as JSON
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'Failed to read notes data' });
-      return;
+      return res.status(500).json({ error: 'Could not read note data' });
     }
     const notes = JSON.parse(data);
     res.json(notes);
@@ -35,58 +34,38 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+  // read the db.json file and add the new note and save it
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'Failed to read notes data' });
-      return;
+      return res.status(500).json({ error: 'Could not read note data' });
     }
 
     const notes = JSON.parse(data);
-    const newNote = req.body;
-    newNote.id = generateUniqueID();
+    const newNote = {
+      id: generateUniqueId(),
+      title: req.body.title,
+      text: req.body.text,
+    };
 
     notes.push(newNote);
 
-    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), 'utf8', (err) => {
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes), 'utf8', (err) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ error: 'Failed to save note' });
-        return;
+        return res.status(500).json({ error: 'Note did not save' });
       }
+
       res.json(newNote);
     });
   });
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const noteId = req.params.id;
-  
-  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to read notes data' });
-      return;
-    }
-
-    const notes = JSON.parse(data);
-    const updatedNotes = notes.filter((note) => note.id !== noteId);
-
-    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(updatedNotes), 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Failed to delete note' });
-        return;
-      }
-      res.sendStatus(204);
-    });
-  });
-});
-
-function generateUniqueID() {
-  return Date.now().toString();
+// generate a unique ID for the new note
+function generateUniqueId() {
+  return Math.random().toString(36).substr(2, 9);
 }
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`App listening on PORT ${PORT}`);
 });
